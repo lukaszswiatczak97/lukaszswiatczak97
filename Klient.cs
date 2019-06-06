cc5c98a53b5c0a1386ad132733323fa88593c276
DawidKubiak <manikes97@gmail.com>
2019-06-06 20:06:51 +0200

    Dawid Kubiak

--- Klient.cs
+++ Klient.cs
@@ -1,21 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+From 27dc67dc8950cb67bc0572ebfdf1ad664881328f Mon Sep 17 00:00:00 2001
+From: "jakub.piotrowski1" <jakub.piotrowski1@unilodz.eu>
+Date: Thu, 6 Jun 2019 17:34:10 +0000
+Subject: [PATCH] Deleted Administrator.cs
 
-namespace ConsoleApp1
-{
-    class Klient
-    {
-        private string imie;
-        private string nazwisko;
-        private string numerTelefonu;
-        Bilet bilet;
+---
+ Administrator.cs | 20 --------------------
+ 1 file changed, 20 deletions(-)
+ delete mode 100644 Administrator.cs
 
-        public string Imie { get => imie; set => imie = value; }
-        public string Nazwisko { get => nazwisko; set => nazwisko = value; }
-        public string NumerTelefonu { get => numerTelefonu; set => numerTelefonu = value; }
-        internal Bilet Bilet { get => bilet; set => bilet = value; }
-    }
-}
+diff --git a/Administrator.cs b/Administrator.cs
+deleted file mode 100644
+index 50122e8..0000000
+--- a/Administrator.cs
++++ /dev/null
+@@ -1,20 +0,0 @@
+-﻿using System;
+-using System.Collections.Generic;
+-using System.Linq;
+-using System.Text;
+-using System.Threading.Tasks;
+-
+-namespace ConsoleApp1
+-{
+-    class Administrator
+-    {
+-        private string imie;
+-        private string nazwisko;
+-        private int ID;
+-
+-        public string Imie { get => imie; set => imie = value; }
+-        public string Nazwisko { get => nazwisko; set => nazwisko = value; }
+-        public int ID1 { get => ID; set => ID = value; }
+-
+-    }
+-}
+-- 
+2.20.1.windows.1
+
